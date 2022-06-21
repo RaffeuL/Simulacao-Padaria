438ac6fa06e9148188fcb68722de9c3282ec7835
RaffeuL <rafael91.lisboa@hotmail.com>
2022-06-21 19:35:52 -0300

    adiciona tempo de chegada e atendimento

--- Cliente.py
+++ Cliente.py
@@ -1,7 +1,7 @@
-from msilib.schema import Class
-
-
 class Cliente:
-    def __init__(self, nome):
+    def __init__(self, nome, t_chegada_caixa, t_atem_caixa, t_atem_balcao):
         self.nome = nome
-        self.chegada_balcao = 0
+        self.t_chegada_caixa = t_chegada_caixa
+        self.t_atem_caixa = t_atem_caixa
+        self.t_chegada_balcao = t_chegada_caixa + t_atem_caixa + 0.01
+        self.t_atem_balcao = t_atem_balcao
--- Fila.py
+++ Fila.py
@@ -1,14 +1,16 @@
-def fila_caixa(env, recurso, cliente, chegada):
-    yield env.timeout(chegada)
+def fila_caixa(env, recurso, cliente):
+    yield env.timeout(cliente.t_chegada_caixa)
     with recurso.request() as req:
         yield req
-        print(cliente.nome, 'atendida no caixa em', env.now)
-        cliente.chegada_balcao = env.now
-        yield env.timeout(0.40)
-
-def fila_balcao(env, recurso, cliente, chegada):
-    yield env.timeout(chegada)
+        print(cliente.nome, 'chegou no CAIXA em: ', env.now)
+        yield env.timeout(cliente.t_atem_caixa)
+        print(cliente.nome, 'Terminou o atendimento no CAIXA em: ', env.now)
+        cliente.t_chegada_balcao = env.now
+#Ajeitar essa função aqui
+def fila_balcao(env, recurso, cliente):
+    yield env.timeout(cliente.t_chegada_balcao)
     with recurso.request() as req:
         yield req
-        print(cliente.nome, 'atendida no balcao em ', env.now)
-        yield env.timeout(1)
+        print(cliente.nome, 'chegou no BALCAO em: ', env.now)
+        yield env.timeout(cliente.t_atem_balcao)
+        print(cliente.nome, 'Terminou o atendimento no BALCAO em: ', env.now)
