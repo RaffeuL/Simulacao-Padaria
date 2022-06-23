3afc44542b4274e1936b892d309d6bf1c0814141
RaffeuL <rafael91.lisboa@hotmail.com>
2022-06-23 15:24:53 -0300

    comentários

--- Main.py
+++ Main.py
@@ -6,7 +6,8 @@ def simula_Padaria(qtd_caixas: int , qtd_balconistas: int, clientes=''):
     env = simpy.Environment()
     caixas = simpy.Resource(env, capacity=qtd_caixas)
     balconistas = simpy.Resource(env, capacity=qtd_balconistas)
-
+    
+    #(env, tempo de chegada do cliente no caixa, tempo que ele demorou no caixa, tempo que ele demorou no balcão)
     c1 = Cliente(env, "Carlos", 1, 0.30, 0.40)
     c2 = Cliente(env, "Jose", 1.10, 0.40, 1)
     c3 = Cliente(env, "Matilda", 1.15, 0.20, 0.20)
